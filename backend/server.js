0aadef6a8ba531b68514518b697fd3a9cd9fc671
achurs <achurs017@gmail.com>
2024-09-28 11:54:26 +0530

    Project Done

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import {connectDB} from './config/db.js';
 import productRoute from './routes/product.route.js';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/dist')));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+    })
+}
 
 app.use('/api/products',productRoute);
 
